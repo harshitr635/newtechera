76b746a1a55473e16ef88b64ccd6a839eb5fb315
Harshit Raj <104525615+harshitr635@users.noreply.github.com>
2024-05-28 23:25:42 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks");
+
+function showmenu() { 
+    navLinks.style.right = "0";
+
+}
+
+function hideMenu() { 
+    navLinks.style.right = "-200px";
+}
